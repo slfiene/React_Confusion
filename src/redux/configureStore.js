0b6f5f82bed970d6f8a2736606abb8ddde95641a
slfiene <sean.fiene@nintex.com>
2019-04-10 21:28:11 -0700

    ReactReduxFormValidation

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,5 +1,5 @@
 import { createStore } from 'redux';
-import { Reducer, intialState, initialState} from './reducer';
+import { Reducer, initialState} from './reducer';
 
 export const ConfigureStore = () => {
     const store = createStore(
