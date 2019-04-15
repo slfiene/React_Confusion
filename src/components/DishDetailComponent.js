95746d09d61ea1da31113ed9cb42650e0d9db791
slfiene <sean.fiene@nintex.com>
2019-04-14 21:04:30 -0700

    reduxActions

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -41,8 +41,8 @@ class CommentForm extends Component {
   };
 
   handleSubmit = values => {
-    console.log("Current State is:" + JSON.stringify(values));
-    alert("Current State is:" + JSON.stringify(values));
+    this.toggleModal();
+    this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
   };
   render() {
     return (
@@ -121,7 +121,6 @@ class CommentForm extends Component {
                 type="submit"
                 value="submit"
                 color="primary"
-                // onClick={this.handleSubmit}
               >
                 Submit
               </Button>
@@ -146,7 +145,7 @@ function RenderDish({ dish }) {
   );
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, dishId }) {
   if (comments != null) {
     return (
       <div className="col-12 col-md-5 m1">
@@ -168,7 +167,7 @@ function RenderComments({ comments }) {
             );
           })}
         </ul>
-        <CommentForm />
+        <CommentForm dishId={dishId} addComment={addComment}/>
       </div>
     );
   } else {
@@ -193,7 +192,9 @@ const DishDetail = props => {
         </div>
         <div className="row">
           <RenderDish dish={props.dish} />
-          <RenderComments comments={props.comments} />
+          <RenderComments comments={props.comments}
+              addComment={props.addComment}
+              dishId={props.dish.id} />
         </div>
       </div>
     );
